83e2d0684860921bcee97358a33ea5278f49c1e3
Bianca Netto <biancanettoc@gmail.com>
2023-11-23 17:42:36 -0300

    Melhorias

---
+++ head.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<link rel="icon" href="logo.png" type="image/x-icon">
+		<title>TROPICAL MIX </title>
+		<meta charset="utf-8">		
+		<meta content="width=device-width, initia-scale=1.0, user-scalabre=0" name="viewport">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+	</head>
+</html>
--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 ?>
 
 
+<?php require 'head.php'; ?>
+
 <html>
     <main>
         <div class="container mt-5" id="formulario">
@@ -70,4 +72,5 @@
             </div>
         </div>
     </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </html>
