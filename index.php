0f49528a1e2e5352c7401251bbdc67936aaae3a8
Bianca Netto <biancanettoc@gmail.com>
2023-11-27 22:26:35 -0300

    aaaaaa

--- index.php
+++ index.php
@@ -5,9 +5,23 @@
 ?>
 
 <?php require 'head.php'; ?>
-<body>
-<?php require 'navbar.php'; ?>
 
+<?php require 'navbar.php'; ?>
+<body>
+<!-- 
+<img src="logo.png" class="img-fluid" alt="..."> -->
+<div class="container text-center">
+  <div class="row align-items-center">
+    <div class="col">
+      One of three columns
+    </div>
+    <div class="col">
+      One of three columns
+    </div>
+  </div>
+</div>
+    
+</body>
 
 <?php require 'footer.php'; ?>
 
--- navbar.php
+++ navbar.php
@@ -1,35 +1,30 @@
-
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+<nav class="navbar navbar-expand-lg bg-body-tertiary" id="menu">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">
-      <img src="logo.png" alt="Logo" width="80" height="80" class="d-inline-block align-text-top">
-    </a>
-    <div class="titulo">Tropical Mix</div> 
+    <a class="navbar-brand" href="#"><h3 class="titulo">Tropical Mix</h3></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse d-lg-flex" id="navbarNavDropdown">
-      <ul class="navbar-nav col-lg-6 justify-content-lg-center">
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="#">Inicio</a>
+          <a class="nav-link" href="#">Inicio</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Comprar</a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Produtos</a>
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+            Produtos
+          </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Listar produtos</a></li>
             <li><a class="dropdown-item" href="#">Cadastrar produtos</a></li>
           </ul>
         </li>
       </ul>
-      <i class="bi bi-cart">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-          <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-        </svg>
-      </i>
+      <div class = "icone"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart2" viewBox="0 0 16 16">
+  <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5M3.14 5l1.25 5h8.22l1.25-5H3.14zM5 13a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0m9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0"/>
+</svg> 
     </div>
   </div>
 </nav>
-
